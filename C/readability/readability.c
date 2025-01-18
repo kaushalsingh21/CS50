8878e110c859e215e6c725f281914258f64356e5
kaushalsingh21 <kaushalsingh.vit@gmail.com>
2025-01-18 19:32:43 +0000

    feat: word count

--- C/readability/readability.c
+++ C/readability/readability.c
@@ -9,7 +9,8 @@ int main(void){
     printf("%s", text);
     printf("total number of letters are: %ld\n", strlen(text));
 
-    int letter_count =0;
+    int letter_count = 0;
+    int word_count = 0;
 
     for (int i = 0, len = strlen(text); i < len; i++)
     {
@@ -17,8 +18,13 @@ int main(void){
         {
             letter_count++;
         }
+
+        if ( ( text[i] == ' ' || text[i] == '.' ) && text[i-1] != '.' )
+        {
+            word_count++;
+        }
     }
 
-    printf("actual total number of letters are %d\n", letter_count);
+    printf("Total number of letter are %d and total number of words are %d\n", letter_count, word_count);
     
 }   
