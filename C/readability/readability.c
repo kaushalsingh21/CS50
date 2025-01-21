466a859b614f37ee96e92a19ee588af482f2bd03
kaushalsingh21 <kaushalsingh.vit@gmail.com>
2025-01-21 23:23:00 +0000

    feat: calculate index

--- C/readability/readability.c
+++ C/readability/readability.c
@@ -6,11 +6,13 @@ int main(void){
     printf("Text:");
     char text[2000];
     fgets(text, sizeof(text), stdin);
-    printf("%s", text);
-    printf("total number of letters are: %ld\n", strlen(text));
 
     int letter_count = 0;
     int word_count = 0;
+    int sen_count =0;
+
+    int lenght = strlen(text);
+    printf("total length is %d and last element is %c\n", lenght, text[lenght]);
 
     for (int i = 0, len = strlen(text); i < len; i++)
     {
@@ -23,8 +25,22 @@ int main(void){
         {
             word_count++;
         }
-    }
 
-    printf("Total number of letter are %d and total number of words are %d\n", letter_count, word_count);
+        if ( text[i] == '.' || text[i] == '?' || text[i] == '!' )
+        {
+            sen_count++;
+        }
+    }
+    
+    double L = (letter_count/(double)word_count)*100;
+    double S = (sen_count/(double)word_count)*100;
+
+    int index = 0.0588 * L - 0.296 * S - 15.8;
+
+    printf("L is %f and S is %f\n", L, S);
+
+    printf("index is %d\n", index);
+
+    printf("Total number of letter are %d and total number of words are %d and total number of sentence are %d\n", letter_count, word_count, sen_count);
     
 }   
